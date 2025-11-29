83dce4dc132d21deb3ff30a61ed57c8c6e60cbb8
desiwulan99 <desiwulansari883@gmail.com>
2025-11-29 20:30:57 +0700

    update: perbaikan API hewan

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use(router);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.use(router);
